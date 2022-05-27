7f7f52f0ad356af7b05aaa5f622303a519378566
Vlad232008 <vlad2320082009@gmail.com>
2022-05-27 08:22:00 +0400

    123

--- app/src/main/java/com/example/dotaheroes/MainActivity.kt
+++ app/src/main/java/com/example/dotaheroes/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.dotaheroes
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dotaheroes.json.DotaItem
+import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.*
 import java.io.IOException
@@ -29,8 +31,9 @@ class MainActivity : AppCompatActivity() {
             override fun onResponse(call: Call, response: Response) {
                 val json: String = response.body.string()
                 val moshi = Moshi.Builder().build()
-                val jsonAdapter = moshi.adapter(DotaItem::class.java)
-                val heroes = jsonAdapter.fromJson(json).toString()
+                val listType = Types.newParameterizedType(List::class.java, DotaItem::class.java)
+                val adapter: JsonAdapter<List<DotaItem>> = moshi.adapter(listType)
+                val heroes = adapter.fromJson(json).toString()
             }
             override fun onFailure(call: Call, e: IOException) {
             }
